1d2c76009957993bb127ca6433fe50e727977f7a
Portalion <77tobik77@gmail.com>
2023-11-08 22:34:06 +0100

    add shader and utils

---
+++ Computer Graphics/Triangle 3D/src/Utils.cpp
@@ -0,0 +1,18 @@
+#include "Utils.h"
+#include <iostream>
+
+void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[Opengl Error] (" << error << "): " <<
+            function << " " << file << ":" << line << '\n';
+        return false;
+    }
+    return true;
+}
