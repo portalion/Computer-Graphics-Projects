7cd28cca1d46cfc6cdfd906f7c0604bcbadb5c42
Portalion <77tobik77@gmail.com>
2023-11-09 03:28:21 +0100

    add imgui fps

--- Computer Graphics/Triangle 3D/src/main.cpp
+++ Computer Graphics/Triangle 3D/src/main.cpp
@@ -1,6 +1,10 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
 #include "Utils.h"
 #include <iostream>
 #include <glm/glm.hpp>
@@ -39,9 +43,21 @@ int main(void)
     if (glewInit() != GLEW_OK)
         std::cout << "GLEW ERROR" << std::endl;
 
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.IniFilename = nullptr;
+    io.LogFilename = nullptr;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+    ImGui_ImplOpenGL3_Init();
     
     Shape mainShape;
-
+    const ImGuiViewport* main_viewport = ImGui::GetMainViewport();
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -52,8 +68,23 @@ int main(void)
         GLCall(glClearColor(0.1f, 0.2f, 0.2f, 1.0f));
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x, main_viewport->WorkPos.y), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Fps Display", nullptr, 
+            ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoDecoration |
+            ImGuiWindowFlags_NoInputs | ImGuiWindowFlags_NoDocking
+        );
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+        ImGui::End();
+
         mainShape.Draw();
 
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         /* Swap front and back buffers */
         GLCall(glfwSwapBuffers(window));
 
