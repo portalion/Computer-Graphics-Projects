3598ecb9e41f7a9a5cec6c2cbe1d94e7cc4e3fad
Portalion <77tobik77@gmail.com>
2023-12-17 01:18:48 +0100

    add bezier curves

--- Computer Graphics/ImagesRgbToCmykConverter/Form1.Designer.cs
+++ Computer Graphics/ImagesRgbToCmykConverter/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.MagentaImage = new System.Windows.Forms.PictureBox();
             this.BlackAndWhiteImage = new System.Windows.Forms.PictureBox();
             this.YellowImage = new System.Windows.Forms.PictureBox();
+            this.CurrentImage = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
             this.BezierPlot = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
@@ -56,7 +57,7 @@
             this.tableLayoutPanel7 = new System.Windows.Forms.TableLayoutPanel();
             this.BlackAdnWhiteButton = new System.Windows.Forms.Button();
             this.SavePicturesButton = new System.Windows.Forms.Button();
-            this.CurrentImage = new System.Windows.Forms.PictureBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
@@ -64,13 +65,13 @@
             ((System.ComponentModel.ISupportInitialize)(this.MagentaImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.BlackAndWhiteImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.YellowImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CurrentImage)).BeginInit();
             this.tableLayoutPanel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.BezierPlot)).BeginInit();
             this.tableLayoutPanel5.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel6.SuspendLayout();
             this.tableLayoutPanel7.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.CurrentImage)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -102,7 +103,6 @@
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.Size = new System.Drawing.Size(486, 555);
             this.tableLayoutPanel2.TabIndex = 0;
-            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
             // 
             // tableLayoutPanel3
             // 
@@ -128,6 +128,7 @@
             this.CyanImage.Location = new System.Drawing.Point(3, 3);
             this.CyanImage.Name = "CyanImage";
             this.CyanImage.Size = new System.Drawing.Size(234, 130);
+            this.CyanImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.CyanImage.TabIndex = 0;
             this.CyanImage.TabStop = false;
             // 
@@ -137,6 +138,7 @@
             this.MagentaImage.Location = new System.Drawing.Point(243, 3);
             this.MagentaImage.Name = "MagentaImage";
             this.MagentaImage.Size = new System.Drawing.Size(234, 130);
+            this.MagentaImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.MagentaImage.TabIndex = 1;
             this.MagentaImage.TabStop = false;
             // 
@@ -146,6 +148,7 @@
             this.BlackAndWhiteImage.Location = new System.Drawing.Point(243, 139);
             this.BlackAndWhiteImage.Name = "BlackAndWhiteImage";
             this.BlackAndWhiteImage.Size = new System.Drawing.Size(234, 130);
+            this.BlackAndWhiteImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.BlackAndWhiteImage.TabIndex = 2;
             this.BlackAndWhiteImage.TabStop = false;
             // 
@@ -155,8 +158,19 @@
             this.YellowImage.Location = new System.Drawing.Point(3, 139);
             this.YellowImage.Name = "YellowImage";
             this.YellowImage.Size = new System.Drawing.Size(234, 130);
+            this.YellowImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.YellowImage.TabIndex = 3;
             this.YellowImage.TabStop = false;
+            // 
+            // CurrentImage
+            // 
+            this.CurrentImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CurrentImage.Location = new System.Drawing.Point(3, 3);
+            this.CurrentImage.Name = "CurrentImage";
+            this.CurrentImage.Size = new System.Drawing.Size(480, 271);
+            this.CurrentImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CurrentImage.TabIndex = 1;
+            this.CurrentImage.TabStop = false;
             // 
             // tableLayoutPanel4
             // 
@@ -181,6 +195,9 @@
             this.BezierPlot.Size = new System.Drawing.Size(480, 392);
             this.BezierPlot.TabIndex = 0;
             this.BezierPlot.TabStop = false;
+            this.BezierPlot.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BezierPlot_MouseDown);
+            this.BezierPlot.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BezierPlot_MouseMove);
+            this.BezierPlot.MouseUp += new System.Windows.Forms.MouseEventHandler(this.BezierPlot_MouseUp);
             // 
             // tableLayoutPanel5
             // 
@@ -208,6 +225,7 @@
             this.flowLayoutPanel1.Controls.Add(this.BlackRadio);
             this.flowLayoutPanel1.Controls.Add(this.ShowAllCheckbox);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(114, 145);
@@ -221,8 +239,10 @@
             this.CyanRadio.Size = new System.Drawing.Size(52, 19);
             this.CyanRadio.TabIndex = 0;
             this.CyanRadio.TabStop = true;
+            this.CyanRadio.Tag = "0";
             this.CyanRadio.Text = "Cyan";
             this.CyanRadio.UseVisualStyleBackColor = true;
+            this.CyanRadio.CheckedChanged += new System.EventHandler(this.Radio_CheckedChanged);
             // 
             // MagentaRadio
             // 
@@ -232,8 +252,10 @@
             this.MagentaRadio.Size = new System.Drawing.Size(72, 19);
             this.MagentaRadio.TabIndex = 1;
             this.MagentaRadio.TabStop = true;
+            this.MagentaRadio.Tag = "1";
             this.MagentaRadio.Text = "Magenta";
             this.MagentaRadio.UseVisualStyleBackColor = true;
+            this.MagentaRadio.CheckedChanged += new System.EventHandler(this.Radio_CheckedChanged);
             // 
             // YellowRadio
             // 
@@ -243,9 +265,10 @@
             this.YellowRadio.Size = new System.Drawing.Size(59, 19);
             this.YellowRadio.TabIndex = 2;
             this.YellowRadio.TabStop = true;
+            this.YellowRadio.Tag = "2";
             this.YellowRadio.Text = "Yellow";
             this.YellowRadio.UseVisualStyleBackColor = true;
-            this.YellowRadio.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            this.YellowRadio.CheckedChanged += new System.EventHandler(this.Radio_CheckedChanged);
             // 
             // BlackRadio
             // 
@@ -255,8 +278,10 @@
             this.BlackRadio.Size = new System.Drawing.Size(68, 19);
             this.BlackRadio.TabIndex = 3;
             this.BlackRadio.TabStop = true;
+            this.BlackRadio.Tag = "3";
             this.BlackRadio.Text = "(K)Black";
             this.BlackRadio.UseVisualStyleBackColor = true;
+            this.BlackRadio.CheckedChanged += new System.EventHandler(this.Radio_CheckedChanged);
             // 
             // ShowAllCheckbox
             // 
@@ -267,6 +292,7 @@
             this.ShowAllCheckbox.TabIndex = 4;
             this.ShowAllCheckbox.Text = "Show all curves";
             this.ShowAllCheckbox.UseVisualStyleBackColor = true;
+            this.ShowAllCheckbox.CheckedChanged += new System.EventHandler(this.ShowAllCheckbox_CheckedChanged);
             // 
             // tableLayoutPanel6
             // 
@@ -301,7 +327,6 @@
             this.NoBackingButton.TabIndex = 0;
             this.NoBackingButton.Text = "0% cofnięcia";
             this.NoBackingButton.UseVisualStyleBackColor = true;
-            this.NoBackingButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // ShowAllButton
             // 
@@ -312,6 +337,7 @@
             this.ShowAllButton.TabIndex = 1;
             this.ShowAllButton.Text = "Show all pictures";
             this.ShowAllButton.UseVisualStyleBackColor = true;
+            this.ShowAllButton.Click += new System.EventHandler(this.ShowAllButton_Click);
             // 
             // FullBackButton
             // 
@@ -332,6 +358,7 @@
             this.ChangePictureButton.TabIndex = 3;
             this.ChangePictureButton.Text = "Change picture";
             this.ChangePictureButton.UseVisualStyleBackColor = true;
+            this.ChangePictureButton.Click += new System.EventHandler(this.ChangePictureButton_Click);
             // 
             // UCRButton
             // 
@@ -408,14 +435,9 @@
             this.SavePicturesButton.Text = "Save Pictures";
             this.SavePicturesButton.UseVisualStyleBackColor = true;
             // 
-            // CurrentImage
+            // openFileDialog
             // 
-            this.CurrentImage.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.CurrentImage.Location = new System.Drawing.Point(3, 3);
-            this.CurrentImage.Name = "CurrentImage";
-            this.CurrentImage.Size = new System.Drawing.Size(480, 271);
-            this.CurrentImage.TabIndex = 1;
-            this.CurrentImage.TabStop = false;
+            this.openFileDialog.FileName = "openFileDialog";
             // 
             // CMYKForm
             // 
@@ -423,6 +445,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 561);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "CMYKForm";
             this.Text = "CMYK";
             this.tableLayoutPanel1.ResumeLayout(false);
@@ -432,6 +455,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.MagentaImage)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.BlackAndWhiteImage)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.YellowImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CurrentImage)).EndInit();
             this.tableLayoutPanel4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.BezierPlot)).EndInit();
             this.tableLayoutPanel5.ResumeLayout(false);
@@ -439,7 +463,6 @@
             this.flowLayoutPanel1.PerformLayout();
             this.tableLayoutPanel6.ResumeLayout(false);
             this.tableLayoutPanel7.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.CurrentImage)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -475,5 +498,6 @@
         private Button BlackAdnWhiteButton;
         private Button SavePicturesButton;
         private PictureBox CurrentImage;
+        private OpenFileDialog openFileDialog;
     }
 }
--- Computer Graphics/ImagesRgbToCmykConverter/Form1.cs
+++ Computer Graphics/ImagesRgbToCmykConverter/Form1.cs
@@ -1,35 +1,165 @@
+using System.Net.Http.Headers;
+using System.Windows.Forms;
+
 namespace ImagesRgbToCmykConverter
 {
     public partial class CMYKForm : Form
     {
+        public Image? mainImage;
+        public Bitmap? mainImageBitmap;
+        public Bitmap bezierPlotsBitmap;
+        public List<List<Point>> bezierControlPoints;
+        public List<Color> colorList;
+        public int ActiveControlPoint = -1;
+
+        public int ActiveIndex { get; set; }
+
+        public void DrawLine(int index, Color color)
+        {
+            Graphics gBmp = Graphics.FromImage(bezierPlotsBitmap);
+            gBmp.DrawBeziers(new Pen(color), bezierControlPoints[index].ToArray());
+            if (index != ActiveIndex)
+                return;
+
+            foreach (var point in bezierControlPoints[index])
+                gBmp.DrawEllipse(new Pen(Color.Black),
+                    point.X - 5, point.Y - 5, 10, 10);
+        }
+
+        public void DrawPlot()
+        {
+            bezierPlotsBitmap = new Bitmap(BezierPlot.Width, BezierPlot.Height);
+            BezierPlot.Image = bezierPlotsBitmap;
+            Graphics gBmp = Graphics.FromImage(bezierPlotsBitmap);
+            gBmp.FillRectangle(new SolidBrush(Color.DarkGray), 0, 0, Width, Height);
+            gBmp.DrawLine(new Pen(Color.Black), 6, 6, 6, bezierPlotsBitmap.Height - 6);
+            gBmp.DrawLine(new Pen(Color.Black), 6, bezierPlotsBitmap.Height - 6, bezierPlotsBitmap.Width, bezierPlotsBitmap.Height - 6);
+
+            if (ShowAllCheckbox.Checked)
+            {
+                for (int i = 0; i < 4; i++)
+                    DrawLine(i, colorList[i]);
+            }
+            else
+                DrawLine(ActiveIndex, colorList[ActiveIndex]);
+        }
+
+        public void InitializePlot()
+        { 
+            bezierControlPoints = new List<List<Point>>();
+            for(int i = 0; i < 4; i++ )
+            {
+                List<Point> controlPoints = new List<Point>()
+                {
+                    new Point(6, bezierPlotsBitmap.Height - 6),
+                    new Point(bezierPlotsBitmap.Width / 3, bezierPlotsBitmap.Height / 3 * 2),
+                    new Point(bezierPlotsBitmap.Width / 3 * 2, bezierPlotsBitmap.Height/ 3),
+                    new Point(bezierPlotsBitmap.Width - 6, 6)
+                };
+                bezierControlPoints.Add(controlPoints);
+            }
+            DrawPlot();
+        }
+
         public CMYKForm()
         {
+            colorList = new List<Color>()
+            {
+                Color.Cyan, Color.Magenta, Color.Yellow, Color.Black
+            };
+            ActiveIndex = 0;
             InitializeComponent();
+            bezierPlotsBitmap = new Bitmap(BezierPlot.Width, BezierPlot.Height);
+            BezierPlot.Image = bezierPlotsBitmap;
+            InitializePlot();
         }
 
-        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        private void ChangePictureButton_Click(object sender, EventArgs e)
         {
+            openFileDialog.Filter = "Image Files (*.jpg)|*.jpg|(*.png)|*.png|(*.gif)|*.gif|(*.jpeg)|*.jpeg|(*.bmp)|*.bmp";
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string filepath = openFileDialog.FileName;
+                mainImage = Image.FromFile(filepath);
+                CurrentImage.Image = mainImage;
+                mainImageBitmap = new Bitmap(mainImage);
+            }
+        }
+
+        private Bitmap GenerateBitmapFromRules(Color color) 
+        {
+            if (mainImageBitmap is null) throw new InvalidDataException();
+            Bitmap result = new Bitmap(mainImageBitmap);
+            for (int i = 0; i < result.Width; i++)
+                for (int j = 0; j < result.Height; j++)
+                    result.SetPixel(i, j, color);
+            return result;
+        }
+
+        private void ShowAllButton_Click(object sender, EventArgs e)
+        {
+            if (mainImage is null) return;
+            CyanImage.Image = GenerateBitmapFromRules(Color.RebeccaPurple);
+            MagentaImage.Image = GenerateBitmapFromRules(Color.BlueViolet);
+            YellowImage.Image = GenerateBitmapFromRules(Color.GreenYellow);
+            BlackAndWhiteImage.Image = GenerateBitmapFromRules(Color.Black);
 
         }
 
-        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        private void BezierPlot_MouseDown(object sender, MouseEventArgs e)
         {
-
+            for(int i = 0; i < bezierControlPoints[ActiveIndex].Count; i++)
+            {
+                var point = bezierControlPoints[ActiveIndex][i];
+                if (e.Location.X > point.X - 6 && e.Location.X < point.X + 6 &&
+                    e.Location.Y > point.Y - 6 && e.Location.Y < point.Y + 6)
+                {
+                    ActiveControlPoint = i;
+                    return;
+                }
+            }
+            ActiveControlPoint = -1;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void BezierPlot_MouseUp(object sender, MouseEventArgs e)
         {
-
+            ActiveControlPoint = -1;
         }
 
-        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        private void Radio_CheckedChanged(object sender, EventArgs e)
         {
+            RadioButton snd = sender as RadioButton;
+            ActiveIndex = Int32.Parse((string)snd.Tag);
 
+            DrawPlot();
         }
 
-        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        private void ShowAllCheckbox_CheckedChanged(object sender, EventArgs e)
         {
+            DrawPlot();
+        }
 
+        private void BezierPlot_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point toSetUp = new Point(e.Location.X, e.Location.Y);
+            if (toSetUp.X < 6) toSetUp.X = 6;
+            if (toSetUp.X > bezierPlotsBitmap.Width) toSetUp.X = bezierPlotsBitmap.Width;
+            if (toSetUp.Y < 6) toSetUp.Y = 6;
+            if (toSetUp.Y > bezierPlotsBitmap.Height) toSetUp.Y = bezierPlotsBitmap.Height;
+
+            if (ActiveControlPoint == -1) return;
+            else if (ActiveControlPoint == 0 || 
+                ActiveControlPoint == bezierControlPoints[ActiveIndex].Count - 1)
+                bezierControlPoints[ActiveIndex][ActiveControlPoint] =
+                   new Point(
+                        bezierControlPoints[ActiveIndex][ActiveControlPoint].X,
+                        toSetUp.Y
+                    );
+            else
+                bezierControlPoints[ActiveIndex][ActiveControlPoint] = toSetUp;
+            DrawPlot();
         }
     }
 }
