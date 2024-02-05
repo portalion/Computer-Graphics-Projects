baa54e6420f4718b3e58ccc9cdbf081233b568cf
Portalion <77tobik77@gmail.com>
2024-02-05 04:09:25 +0100

    add smooth lining

--- Computer Graphics/SceneViewer/res/shaders/phongLight.fs
+++ Computer Graphics/SceneViewer/res/shaders/phongLight.fs
@@ -16,8 +16,12 @@ uniform Light spotlight;
 uniform vec3 viewPos; 
 uniform vec3 u_Color;
 
+vec3 GetLightning(Light l);
+
 vec3 getPointLight(Light l);
 vec3 getSpotlightLight(Light l);
+
+
 
 void main()
 {
@@ -26,13 +30,8 @@ void main()
     FragColor = vec4(result, 1.0);
 } 
 
-vec3 getPointLight(Light l)
+vec3 GetLightning(Light l)
 {
-    // ambient
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * l.color;
-  	
-    // diffuse 
     vec3 lightDir = normalize(l.position - FragPos);
     float diff = max(dot(Normal, lightDir), 0.0);
     vec3 diffuse = diff * l.color;
@@ -42,9 +41,17 @@ vec3 getPointLight(Light l)
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, Normal);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * l.color;  
+    vec3 specular = specularStrength * spec * l.color; 
+    return diffuse + specular;
+}
 
-    return (ambient + diffuse + specular);
+vec3 getPointLight(Light l)
+{
+    // ambient
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * l.color;
+
+    return ambient + GetLightning(l);
 }
 
 vec3 getSpotlightLight(Light l)
@@ -53,31 +60,9 @@ vec3 getSpotlightLight(Light l)
     
     // check if lighting is inside the spotlight cone
     float theta = dot(lightDir, normalize(-l.direction)); 
-    
-    if(theta > cos(radians(12.5f))) // remember that we're working with angles as cosines instead of degrees so a '>' is used.
-    {    
-        // ambient
-         float ambientStrength = 0.1;
-         vec3 ambient = ambientStrength * l.color;
-  	        
-         // diffuse 
-         vec3 lightDir = normalize(l.position - FragPos);
-         float diff = max(dot(Normal, lightDir), 0.0);
-         vec3 diffuse = diff * l.color;
-         
-         // specular
-         float specularStrength = 0.5;
-         vec3 viewDir = normalize(viewPos - FragPos);
-         vec3 reflectDir = reflect(-lightDir, Normal);  
-         float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-         vec3 specular = specularStrength * spec * l.color;  
+    float epsilon   = cos(radians(12.5f)) - cos(radians(17.5f));
+    float intensity = clamp((theta - cos(radians(17.5f))) / epsilon, 0.0, 1.0);    
 
-         return (ambient + diffuse + specular);
-    }
-    else 
-    {
-        // else, use ambient light so scene isn't completely dark outside the spotlight.
-        return vec3(0.1f, 0.1f, 0.1f);
-    }
+    return 0.1 * l.color + GetLightning(l) * intensity;
 }
 
